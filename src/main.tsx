411e3f24a54107cec394534e9e0ce995606f899c
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-10-21 10:36:14 -0300

    fix(swiper): change directory of imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import "swiper/swiper.min.css";
+import "swiper/css/pagination.min.css";
+import "swiper/css/navigation.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
