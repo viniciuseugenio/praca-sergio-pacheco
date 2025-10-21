1760af074bb0d85f2d041fcad0b54f1b415a7b91
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-10-21 10:40:41 -0300

    refactor(swiper): move imports to index.css file

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "swiper/swiper.min.css";
-import "swiper/css/pagination.min.css";
-import "swiper/css/navigation.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
