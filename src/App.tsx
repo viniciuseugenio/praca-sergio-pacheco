99826e8b0c4842d76ca1fb21b8b49018a5d07ca1
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-10-22 15:24:33 -0300

    feat(history): display History component on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
 import HeroSection from "./components/HeroSection";
+import History from "./components/History";
 import HowToUse from "./components/HowToUse";
 import IncomingEvents from "./components/IncomingEvents";
 
@@ -11,6 +12,7 @@ function App() {
       <HowToUse />
       <Gallery />
       <IncomingEvents />
+      <History />
       <Footer />
     </>
   );
