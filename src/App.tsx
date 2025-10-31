d4bb2bef9e842c37070ce35cce9b0cf004281048
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-10-31 11:45:15 -0300

    feat(motion): add MotionConfig tag to modify the default transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { MotionConfig } from "motion/react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
 
 function App() {
   return (
+    <MotionConfig
+      transition={{
+        type: "spring",
+        duration: 0.8,
+        ease: "easeInOut",
+        bounce: 0,
+      }}
+    >
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
@@ -11,6 +20,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+    </MotionConfig>
   );
 }
 
