98834139d44c1421ae61bd08ce8892819061577e
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-11-13 21:09:10 -0300

    fix(layout): open page at the beginning in URL change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout: React.FC = () => {
         }
       }, 100);
     } else {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo(0, 0);
     }
   }, [pathname, hash]);
 
