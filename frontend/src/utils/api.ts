b19a518917f114becbb4620178ffe9e5da7dbed4
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-12-01 15:52:29 -0300

    fix(api): update API_BASE_URL to remote back-end

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://sergio-pacheco-app-iyhrl.ondigitalocean.app/api";
 
 interface FetchOptions extends RequestInit {
   requiresAuth?: boolean;
