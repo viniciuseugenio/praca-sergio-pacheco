0062db08da8be961ffddc96fe1aaa89a903d7ff0
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-12-02 17:35:20 -0300

    fix(views): return response in update and create views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
+from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.permissions import AllowAny, IsAdminUser
-from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+
 from .models import Event, NatureElement
 from .serializers import EventSerializer, NatureElementSerializer
 
@@ -8,9 +9,9 @@ from .serializers import EventSerializer, NatureElementSerializer
 class EventListCreateView(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    
+
     def get_permissions(self):
-        if self.request.method == 'POST':
+        if self.request.method == "POST":
             return [IsAdminUser()]
         return [AllowAny()]
 
@@ -18,9 +19,9 @@ class EventListCreateView(generics.ListCreateAPIView):
 class EventRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    
+
     def get_permissions(self):
-        if self.request.method in ['GET']:
+        if self.request.method in ["GET"]:
             return [AllowAny()]
         return [IsAdminUser()]
 
@@ -29,19 +30,26 @@ class NatureElementListCreateView(generics.ListCreateAPIView):
     queryset = NatureElement.objects.all()
     serializer_class = NatureElementSerializer
     parser_classes = (MultiPartParser, FormParser, JSONParser)
-    
+
     def get_permissions(self):
-        if self.request.method == 'POST':
+        if self.request.method == "POST":
             return [IsAdminUser()]
         return [AllowAny()]
+
+        response = super().create(request, *args, **kwargs)
+        return response
 
 
 class NatureElementRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = NatureElement.objects.all()
     serializer_class = NatureElementSerializer
     parser_classes = (MultiPartParser, FormParser, JSONParser)
-    
+
     def get_permissions(self):
-        if self.request.method in ['GET']:
+        if self.request.method in ["GET"]:
             return [AllowAny()]
         return [IsAdminUser()]
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return response
